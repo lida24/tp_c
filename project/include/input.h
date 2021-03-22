4a1382c6b7ef6f8c6bd9e586ed49cd2ec667fec5
lida24 <lidalenkova204@gmail.com>
2021-03-23 00:51:33 +0300

    f

--- project/include/input.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdio.h>
-#include <ctype.h>
-
-#define DATA_ERR 200
-#define MAX_STR_SIZE 100
-
-int parse_application(char buffer[MAX_STR_SIZE], char* name, char* function, int* version, int* install_day, int* install_month, int* install_year, int* update_day, int* update_month, int* update_year);
-
