f375535d6d8f0a6b5d4c5e547062f30de89f7cf5
lida24 <lidalenkova204@gmail.com>
2021-04-01 19:18:45 +0300

    travis1

--- iz2/src/stat.c
+++ iz2/src/stat.c
@@ -1,7 +1,5 @@
 #include"stat.h"
 void count(int* counters, uint8_t* fileData, long fileLength, int num){
- printf("\n\n\n\n\n %d \n\n\n\n\n", num);
- printf("\n\n\n\n\n %ld \n\n\n\n\n", fileLength);
     for ( int i = 0; i < num; ++i ) 
         for ( long j = 0; j < fileLength - 1; ++j )
             if ( abs(fileData[j] - fileData[j + 1]) == i )
