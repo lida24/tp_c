eb25fffba55cf1825dbccffc03788224a86bfa15
lida24 <lidalenkova204@gmail.com>
2021-04-02 23:03:21 +0300

    travis

---
+++ iz2/cmakebuild/CMakeFiles/test_parallel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_parallel.dir/src/dyn.c.o"
+  "CMakeFiles/test_parallel.dir/tests/tests.cpp.o"
+  "test_parallel"
+  "test_parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test_parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
