91ae4987fc341237ff804a946292b8bf3ea901fb
lida24 <lidalenkova204@gmail.com>
2021-04-02 22:36:12 +0300

    cmake

---
+++ iz2/cmakebuild/CMakeFiles/parallel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel.dir/src/main.c.o"
+  "parallel"
+  "parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ iz2/cmakebuild/CMakeFiles/test_stat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_stat.dir/src/stat.c.o"
+  "CMakeFiles/test_stat.dir/tests/tests.cpp.o"
+  "test_stat"
+  "test_stat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test_stat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
