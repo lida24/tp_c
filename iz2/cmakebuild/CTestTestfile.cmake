5c52039e00419728b0693d17a0a11c523947ac93
lida24 <lidalenkova204@gmail.com>
2021-04-02 20:06:39 +0300

    rm dir

--- iz2/cmakebuild/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lida/TechnoPark/yz/tp_c/iz2
-# Build directory: /home/lida/TechnoPark/yz/tp_c/iz2/cmakebuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
